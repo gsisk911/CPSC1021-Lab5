5bc90eb63c386cf0e5df43a60717602d6d38d637
Sam Crooks <samcrooks@99.238.21.198.tigernet.wifi.dyn.clemson.edu>
2022-03-04 10:59:08 -0500

    sam main

--- src/ParkingPermits.cpp
+++ src/ParkingPermits.cpp
@@ -2,12 +2,14 @@
 #include <sstream>
 #include "student.h"
 #include "car.h"
+#include "Visitor.h"
+#include "Moped.h"
 
 using namespace std;
 
-enum customerType { employee = 1, student, visitor, vendor};
+enum customerType { employee = 1, student, visitor = 3, vendor};
 enum vehType { car = 1, lev, moped, motorcycle};
-enum passType { commuterP = 1, residentP, teacherP, visitorP, vendorP, motorcycleP };
+enum passType { commuterP, residentP, teacherP, visitorP, vendorP, motorcycleP };
 
 int makeInvoice();
 
@@ -128,6 +130,33 @@ int main()
                 }
                 case moped:
                 {
+                    Moped moped;
+
+                    string make;
+                    cout << "What is your vehicle make?" << endl;
+                    std::cin >> make;
+                    moped.setMake(make);
+
+                    string model;
+                    cout << "What is your vehicle model?" << endl;
+                    std::cin >> model;
+                    moped.setModel(model);
+
+                    string color;
+                    cout << "What is your vehicle color?" << endl;
+                    std::cin >> color;
+                    moped.setColor(color);
+
+                    bool inFrat;
+                    cout << "Are you in a Fraterity? (1 for yes, 0 for no)" << endl;
+                    cin >> inFrat;
+                    moped.setInFrat(inFrat);
+
+                    int engineSize;
+                    cout << "What is the size of your engine (cc's)" << endl;
+                    cin >> engineSize;
+                    moped.setEngineSize(engineSize);
+
                     break;
                 }
                 case motorcycle:
@@ -140,9 +169,143 @@ int main()
                     return 1;
                 }
             }
+            break;
         }
         case visitor:
         {
+            Visitor visitor;
+
+            string nameF;
+            string nameL;
+            cout << "What is your first name?" << endl;
+            cin >> nameF;
+            cout << "What is your last name?" << endl;
+            cin >> nameL;
+            visitor.setName(nameF+" "+nameL);
+
+            cout << visitor.getName() << endl;
+
+            std::cin.ignore();
+            string address;
+            cout << "What is your address?" << endl;
+            getline(cin, address);
+            visitor.setAddress(address);
+
+            cout << visitor.getAddress() << endl;
+
+            string email;
+            cout << "What is your email address?" << endl;
+            cin >> email;
+            visitor.setEmail(email);
+
+            cout << visitor.getEmail() << endl;
+
+            int days;
+            cout << "How many days will you be visiting?" << endl;
+            cin >> days;
+            visitor.setDays(days);
+
+            cout << visitor.getDays() << endl;
+
+            bool onTour;
+            cout << "Are you on a tour of Clemson? (1 for yes, 0 for no)" << endl;
+            cin >> onTour;
+            visitor.setOnTour(onTour);
+
+            cout << visitor.getOnTour() << endl;
+
+            cout << "Do you drive a car/truck/suv(1), low-emission vehicle(2), moped(3), or motorcycle(4)?" << endl;
+            std::cin >> buf;
+            userVeh = (vehType)buf;
+
+            switch (userVeh)
+            {
+                case car:
+                {
+                    Car *userCar = new Car;
+
+                    string make;
+                    cout << "What is your vehicle make?" << endl;
+                    std::cin >> make;
+                    userCar->setMake(make);
+
+                    cout << userCar->getMake() << endl;
+
+                    string model;
+                    cout << "What is your vehicle model?" << endl;
+                    std::cin >> model;
+                    userCar->setModel(model);
+
+                    cout << userCar->getModel() << endl;
+
+                    string color;
+                    cout << "What is your vehicle color?" << endl;
+                    std::cin >> color;
+                    userCar->setColor(color);
+
+                    cout << userCar->getColor() << endl;
+
+                    int year;
+                    cout << "What is your vehicle year?" << endl;
+                    std::cin >> year;
+                    userCar->setYear(year);
+
+                    cout << userCar->getYear() << endl;
+
+                    double zeroToSixty;
+                    cout << "What is your vehicle's 0-60 time in seconds?" << endl;
+                    std::cin >> zeroToSixty;
+                    userCar->setIsSlow(zeroToSixty);
+                    
+                    cout << userCar->getIsSlow() << endl;
+
+                }
+                case lev:
+                {
+                    break;
+                }
+                case moped:
+                {
+                    Moped moped;
+
+                    string make;
+                    cout << "What is your vehicle make?" << endl;
+                    std::cin >> make;
+                    moped.setMake(make);
+
+                    string model;
+                    cout << "What is your vehicle model?" << endl;
+                    std::cin >> model;
+                    moped.setModel(model);
+
+                    string color;
+                    cout << "What is your vehicle color?" << endl;
+                    std::cin >> color;
+                    moped.setColor(color);
+
+                    bool inFrat;
+                    cout << "Are you in a Fraterity? (1 for yes, 0 for no)" << endl;
+                    cin >> inFrat;
+                    moped.setInFrat(inFrat);
+
+                    int engineSize;
+                    cout << "What is the size of your engine (cc's)" << endl;
+                    cin >> engineSize;
+                    moped.setEngineSize(engineSize);
+
+                    break;
+                }
+                case motorcycle:
+                {
+                    break;
+                }
+                default:
+                {
+                    cout << "Unknown Input" << endl;
+                    return 1;
+                }
+            }
+
             break;
         }
         case vendor:
